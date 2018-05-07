ec694599ef12cd3aecf197ab843c3379a2981876
ssanchez59 <s.sanchez59@uniandes.edu.co>
2018-05-07 11:25:42 -0500

    client

---
+++ client/src/components/App.js
@@ -0,0 +1,8 @@
+//React Router stuff
+import React from 'react';
+
+const App = () => {
+  return <div>ABET Uniandes</div>;
+};
+
+export default App;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 module.exports = app => {
   app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
+    res.send({ abet: 'uniandes' });
   });
 };
